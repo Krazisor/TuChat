e81d48b496109d47200b50a18c454bffe79ae703
haoranTu <1642804440@qq.com>
2025-05-23 16:16:37 +0800

    feat: 新增minio对外暴露临时接口的api

--- src/main/java/com/thr/tuchat/service/MinioService.java
+++ src/main/java/com/thr/tuchat/service/MinioService.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
@@ -23,19 +26,35 @@ public class MinioService {
     private final MinioClient minioClient;
     private final MinioConfig minioConfig;
 
-    public String getTemporaryURL(String url) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
-        String filePath = Optional.ofNullable(url)
-                .filter(u -> u.contains("/"))
-                .map(u -> u.substring(u.lastIndexOf('/') + 1))
-                .orElse("");
-        return minioClient.getPresignedObjectUrl(
-                GetPresignedObjectUrlArgs.builder()
-                        .method(Method.GET)
-                        .bucket(minioConfig.getBucketName())
-                        .object(filePath)
-                        .expiry(60 * 60 * 2)
-                        .build()
-        );
+    /**
+     * 获取临时访问链接
+     * --------------只对minio的相关数据提供临时访问连接，其他来源的不提供----------------
+     *
+     * @param url 永久链接
+     * @return String 临时链接
+     */
+    public String getTemporaryURL(String url) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException, URISyntaxException {
+        URL input = new URI(url).toURL();
+        String protocol = input.getProtocol();
+        String host = input.getHost();
+        StringBuilder base = new StringBuilder();
+        base.append(protocol).append("://").append(host);
+        if (!base.toString().equals(minioConfig.getEndpoint())) {
+            return url;
+        } else {
+            String filePath = Optional.ofNullable(url)
+                    .filter(u -> u.contains("/"))
+                    .map(u -> u.substring(u.lastIndexOf('/') + 1))
+                    .orElse("");
+            return minioClient.getPresignedObjectUrl(
+                    GetPresignedObjectUrlArgs.builder()
+                            .method(Method.GET)
+                            .bucket(minioConfig.getBucketName())
+                            .object(filePath)
+                            .expiry(60 * 60 * 2)
+                            .build()
+            );
+        }
     }
 
     // 上传文件并返回访问URL
