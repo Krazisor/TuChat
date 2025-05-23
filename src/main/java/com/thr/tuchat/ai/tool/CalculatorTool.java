739d7d8dc12991935800863c668c5a8baa4bd830
haoranTu <1642804440@qq.com>
2025-05-23 16:13:41 +0800

    feat: 由langchain4j修改为springAI结构

--- src/main/java/com/thr/tuchat/ai/tool/CalculatorTool.java
+++ src/main/java/com/thr/tuchat/ai/tool/CalculatorTool.java
@@ -1,16 +1,16 @@
-package com.thr.tuchat.ai.tool;
-
-import dev.langchain4j.agent.tool.Tool;
-
-public class CalculatorTool {
-
-    @Tool
-    double add(int a, int b) {
-        return a + b;
-    }
-
-    @Tool
-    double squareRoot(double x) {
-        return Math.sqrt(x);
-    }
-}
+//package com.thr.tuchat.ai.tool;
+//
+//import dev.langchain4j.agent.tool.Tool;
+//
+//public class CalculatorTool {
+//
+//    @Tool
+//    double add(int a, int b) {
+//        return a + b;
+//    }
+//
+//    @Tool
+//    double squareRoot(double x) {
+//        return Math.sqrt(x);
+//    }
+//}
--- src/test/java/com/thr/tuchat/TuchatApplicationTests.java
+++ src/test/java/com/thr/tuchat/TuchatApplicationTests.java
@@ -1,13 +1,28 @@
 package com.thr.tuchat;
 
+import com.thr.tuchat.service.AIService;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 @SpringBootTest
 class TuchatApplicationTests {
 
+    @Resource
+    private AIService aiService;
+
     @Test
     void contextLoads() {
+        System.out.println(System.getenv("TEST_KEY"));
+    }
+
+    @Test
+    void TestAIStreamingResponse () {
+        Flux<String> response = aiService.getAIResponse("给我一段python判断素数的代码", "gpt-4.1");
+        response.doOnNext(System.out::print).blockLast(); // 简明调试用
     }
 
 }
